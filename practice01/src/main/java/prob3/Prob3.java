407fa6db4ac388fe4514b3af95ad1b2f407757c7
BIT <BIT@DESKTOP-19PV3J5>
2021-04-27 15:08:49 +0900

    practice01-3

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -3,13 +3,36 @@ package prob3;
 import java.util.Scanner;
 
 public class Prob3 {
-	
+
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
-		
+		System.out.print("숫자를 입력하세요 : ");
+		int number = scanner.nextInt();
+		System.out.println("결과 값 : " + cal(number));
+		System.out.print("숫자를 입력하세요 : ");
+		number = scanner.nextInt();
+		System.out.println("결과 값 : " + cal(number));
+		System.out.print("숫자를 입력하세요 : ");
+		number = scanner.nextInt();
+		System.out.println("결과 값 : " + cal(number));
+
 		scanner.close();
+	}
+
+	public static int cal(int number) {
+		int sum = 0;
+		if (number % 2 == 0) {
+			for (int i = 0; i <= number; i = i + 2)
+				sum += i;
+		} else {
+			for (int i = 1; i <= number; i = i + 2) {
+				sum += i;
+			}
+
+		}
+		return sum;
 	}
 }
