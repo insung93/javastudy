064b9f3e7da8329c1e03b55b9cd21de22a8cebc6
BIT <BIT@DESKTOP-19PV3J5>
2021-04-27 15:08:56 +0900

    practice01-4

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -9,8 +9,12 @@ public class Prob4 {
 
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
+		String out = "";
+		for (int i = 0; i < text.length(); i++) {
+			out += text.charAt(i);
+			System.out.println(out);
+		}
 
-		
 		scanner.close();
 	}
 
