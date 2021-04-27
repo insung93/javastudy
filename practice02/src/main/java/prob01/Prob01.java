ada8b9b4d4879d4b15d7232649ca166e9d77eeb1
BIT <BIT@DESKTOP-19PV3J5>
2021-04-27 15:11:08 +0900

    practice02-1

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
+		Scanner scanner = new Scanner(System.in);
 
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
-
+		System.out.print("금액 : ");
+		int number = scanner.nextInt();
+		for (int i = 0; i < 10; i++) {
+			int x = number/MONEYS[i];
+			if(x!=0) {
+				System.out.println(MONEYS[i]+"원 : " +x+"개");
+				number-=(MONEYS[i]*x);
+			}
+		}
 		/* 코드 작성 */
-		
+
 		scanner.close();
- 	}
+	}
 }
