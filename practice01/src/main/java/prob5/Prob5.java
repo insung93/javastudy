badb165f9514eb327da8da870e18611dcc26196a
BIT <BIT@DESKTOP-19PV3J5>
2021-04-27 15:09:10 +0900

    practice01-5

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,17 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		for (int i = 1; i < 100; i++) {
+			int x = i / 10;
+			int y = i % 10;
+			if (x == 3 || x == 6 || x == 9 || y == 3 || y == 6 || y == 9) {
+				System.out.print(i+" ");
+				if(x == 3 || x == 6 || x == 9 )
+					System.out.print("짝");
+				if(y == 3 || y == 6 || y == 9 )
+					System.out.print("짝");
+				System.out.println();
+			}
+		}
 	}
 }
