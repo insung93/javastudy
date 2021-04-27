3467b94dd49e030a59b32192f710d07dfdb74e04
BIT <BIT@DESKTOP-19PV3J5>
2021-04-27 15:07:51 +0900

    practice01-2

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -3,5 +3,15 @@ package prob2;
 public class Prob2 {
 	public static void main(String[] args) {
 		/* 코드 작성 */
+		int i = 1;
+
+		for (int j = 1; j < 10; j++) {
+			for (int k = 0; k < 10; k++) {
+				System.out.print(i + k + " ");
+			}
+			i += 1;
+			System.out.println();
+		}
+
 	}
 }
