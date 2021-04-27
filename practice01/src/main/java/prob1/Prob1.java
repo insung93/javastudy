e27d7ab329657efcdc9e6f8ce0d428695dea2295
BIT <BIT@DESKTOP-19PV3J5>
2021-04-27 15:07:27 +0900

    practice01-1

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Prob1 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-		
+		Scanner scanner = new Scanner(System.in);
+
 		/* 코드 작성 */
+		System.out.print("수를 입력하시오 : ");
 		int number = scanner.nextInt();
-		
+		if (number % 3 == 0)
+			System.out.println("3의 배수입니다.");
+
 		scanner.close();
 	}
 }
