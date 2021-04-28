67bceee2b65694837405beb5a3cb037c482e0d61
BIT <BIT@DESKTOP-19PV3J5>
2021-04-28 15:02:38 +0900

    practice04-3 solved

---
+++ practice04/src/main/java/prob03/Unit.java
@@ -0,0 +1,30 @@
+package prob03;
+
+public class Unit {
+	private int x;
+	private int y;
+	
+	void move(int x, int y) {
+		/* 지정된 위치로 이동 */
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	void stop() {
+		/* 현재 위치에 정지 */
+	}
+}
