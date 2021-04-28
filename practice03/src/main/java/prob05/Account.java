b48a32a08b8f7964442a39e4e809c1171a212d3b
BIT <BIT@DESKTOP-19PV3J5>
2021-04-28 13:26:21 +0900

    practice03-5

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -1,4 +1,33 @@
 package prob05;
 
 public class Account {
+	String accountNo;
+	int balance;
+	
+	void save(int input) {
+		balance+=input;
+		System.out.println(accountNo+" 계좌에 " + input + "만원이 입금되었습니다.");
+	}
+	void deposit(int output) {
+		balance-=output;
+		System.out.println(accountNo+" 계좌에 " + output + "만원이 출금되었습니다.");
+	}
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	Account(String accountNo) {
+		this.accountNo = accountNo;
+		this.balance = 0;
+		System.out.println(this.accountNo+" 계죄가 개설되었습니다.");
+	}
+	
 }
