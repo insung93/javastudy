b2b188aa5c54d101bf36e997538120832abb0685
BIT <BIT@DESKTOP-19PV3J5>
2021-04-30 16:47:54 +0900

    Generic, 가변인자

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,7 +1,26 @@
 package prob01;
 
 public class Printer {
-	void println(Object str) {
-		System.out.println(str);
+//	void println(Object str) {
+//		System.out.println(str);
+//	}
+	public <T> void println(T t) {
+		System.out.println(t);
+	}
+
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.print(t);
+			System.out.print(" ");
+		}
+		System.out.print("\n");
+	}
+	
+	public int sum(Integer... nums) {
+		Integer sum = 0;
+		for(Integer i : nums) {
+			sum += i;
+		}
+		return sum;
 	}
 }
